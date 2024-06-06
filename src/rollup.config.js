b3374c7d05bc00fd843452b0c09925fa39db2c64
hamitLicina <hamitlicina@gmail.com>
2024-06-06 21:16:33 +0300

    Config file changed

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -1,22 +1,32 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
 
 export default {
     input: 'src/index.js',
     output: [
         {
             file: 'dist/my-library.cjs.js',
-            format: 'cjs'
+            format: 'cjs',
+            sourcemap: true
         },
         {
             file: 'dist/my-library.esm.js',
-            format: 'es'
+            format: 'es',
+            sourcemap: true
         },
         {
             file: 'dist/my-library.umd.js',
             format: 'umd',
-            name: 'MyLibrary'
+            name: 'MyLibrary',
+            sourcemap: true
         }
     ],
-    plugins: [resolve(), commonjs()]
+    plugins: [
+        resolve(),
+        commonjs(),
+        babel({ babelHelpers: 'bundled' }),
+        terser()
+    ]
 };
